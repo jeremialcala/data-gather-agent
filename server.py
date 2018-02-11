ff8ed216e4dcfd53276b42d469217dd446dec3bf
Jeremi Meneses <jmeneses@novopayment.com>
2018-02-11 10:02:36 -0400

    server fix and underconstruction message

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from os import environ
-from flask import Flask
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def verify():
-	return "Hello World"
+	return render_template("index.html")
 
 
 if __name__ == '__main__':
